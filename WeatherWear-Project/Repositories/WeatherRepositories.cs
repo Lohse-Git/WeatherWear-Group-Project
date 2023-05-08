ceef1ded6e36beaf22b55667e223d0c4e99bcd45
Sebastian Lohse Ingvardsen <seba3929@edu.zealand.dk>
2023-05-08 11:38:55 +0200

    Third push

--- WeatherWear-Project/Repositories/WeatherRepositories.cs
+++ WeatherWear-Project/Repositories/WeatherRepositories.cs
@@ -1,18 +1,23 @@
-﻿using WeatherWear_Project.Models;
+﻿using System.Security.Cryptography.X509Certificates;
+using WeatherWear_Project.Models;
 
 namespace WeatherWear_Project.Repositories
 {
     public class WeatherRepositories
     {
         public int _NextId;
-        public List<ClothingReco> clothingrecos;
+        public List<ClothingReco> clothingReco;
 
         public WeatherRepositories() 
         {
-
+            
         }
 
-
+        public IEnumerable<ClothingReco> GetAll()
+        {
+            if (clothingReco == null || clothingReco.Count() <= 0) throw new ArgumentNullException("No Weather Records found");
+            return clothingReco;
+        }
 
 
 
