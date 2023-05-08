ca27b4320d39a577548c772e9014dcb69975087c
Sebastian Lohse Ingvardsen <seba3929@edu.zealand.dk>
2023-05-08 13:12:33 +0200

    Fourth Push

--- WeatherWear-ProjectTests/Models/ClothingRecoTests.cs
+++ WeatherWear-ProjectTests/Models/ClothingRecoTests.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherWear_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WeatherWear_ProjectTests.Models
+namespace WeatherWear_Project.Models.Tests
 {
-    internal class ClothingRecoTests
+    [TestClass()]
+    public class ClothingRecoTests
     {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
     }
 }
---
+++ WeatherWear-ProjectTests/Repositories/WeatherRepositoriesTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherWear_Project.Repositories;
+
+namespace WeatherWear_ProjectTests.Repositories
+{
+    [TestClass()]
+    class WeatherRepositoriesTest
+    {
+        WeatherRepositoriesTest repository;
+
+        [TestMethod()]
+        public void WeatherRepositoryTest()
+        {
+            repository = new WeatherRepositoriesTest();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            repository = new WeatherRepositories();
+            var listOfWeather = repository.GetAll();
+            Assert.IsNotNull(listOfWeather);
+            Assert.AreEqual(3, countInList);
+        }
+    }
+}
